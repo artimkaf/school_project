fda1935ede9c5656fed2d8022f08aa697e3eb164
pragmawolf <pragmawolf@gmail.com>
2022-11-06 18:09:32 +0300

    Some changes

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
 import sys
 import random
 
-from PyQt6 import QtWidgets, uic
-from PyQt6.QtCore import *
+from PyQt6 import QtWidgets
+# from PyQt6 import uic
+# from PyQt6.QtCore import *
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
 from PyQt6.uic import *
 
-title_names=['Free time chess', 'Бесплатное время шахматы']
+title_names = ['Free time chess', 'Бесплатное время шахматы']
 
-class mainwin(QtWidgets.QMainWindow):
+
+class WindowMain(QtWidgets.QMainWindow):
     def __init__(self):
-        super(mainwin, self).__init__()
-        loadUi('menuwin.ui', self)
+        super(WindowMain, self).__init__()
+        self.settings_button = WindowSettings()
+        loadUi('window-main.ui', self)
 
-        self.setbtn.clicked.connect(self.gotosetbtn)
+        self.settings_button.clicked.connect(self.gotosetbtn)
 
-    def gotosetbtn(self):
-        setbtn = setwin()
-        widget.addWidget(setbtn)
+    def press_settings_button(self):
+        settings_button = WindowSettings()
+        widget.addWidget(self.settings_button)
         widget.setCurrentIndex(widget.currentIndex() + 1)
         #print ('clicked')
         #self.Window = setwin()
 
 
-
-class setwin(QtWidgets.QMainWindow):
+class WindowSettings(QtWidgets.QMainWindow):
     def __init__(self):
-        super(setwin, self).__init__()
-        loadUi('settingswin.ui', self)
+        super(WindowSettings, self).__init__()
+        loadUi('window-settings.ui', self)
         #то что ниже работать не хочет
         '''
         self.exitmenubtn.clicked.connect(self.gotosetbtn)
@@ -39,11 +41,12 @@ class setwin(QtWidgets.QMainWindow):
         widget.setCurrentIndex(widget.currentIndex() + 1)
 '''
 
+
 app = QApplication(sys.argv)
-mainwindow = mainwin()
+windowMain = WindowMain()
 widget = QtWidgets.QStackedWidget()
-widget.addWidget(mainwindow)
-widget.setFixedSize(800,600)
+widget.addWidget(windowMain)
+widget.setFixedSize(800, 600)
 widget.setWindowTitle(random.choice(title_names))
 widget.setWindowIcon(QIcon("chess_piece_king.png"))
 widget.show()
