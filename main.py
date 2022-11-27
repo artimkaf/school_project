d7b677cf8e17a5b2bca6d811d564c6286c46d76c
pragmawolf <pragmawolf@gmail.com>
2022-11-27 14:44:36 +0300

    add chess moves and a lot of bugs =)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import random
+import functools
 
 from PyQt6 import QtWidgets
 from PyQt6.QtWidgets import *
@@ -66,27 +67,54 @@ class WindowPlay(QtWidgets.QMainWindow):
         self.background.setPixmap(self.pixmap)
         self.background.setScaledContents(1)
 
+        self.dirlist = ''  # !!!
+
         self.chess_board()
 
-        self.first_button_click()
+
+
+        # self.first_button_click()
 
         self.more_button.clicked.connect(self.press_more_button)
 
-
-
-
-
-
     def chess_board(self):
+        '''
+        self.chess_board_list = [
+        [self.a8, self.b8, self.c8, self.d8, self.e8, self.f8, self.g8, self.h8],
+        [self.a7, self.b7, self.c7, self.d7, self.e7, self.f7, self.g7, self.h7],
+        [self.a6, self.b6, self.c6, self.d6, self.e6, self.f6, self.g6, self.h6],
+        [self.a5, self.b5, self.c5, self.d5, self.e5, self.f5, self.g5, self.h5],
+        [self.a4, self.b4, self.c4, self.d4, self.e4, self.f4, self.g4, self.h4],
+        [self.a3, self.b3, self.c3, self.d3, self.e3, self.f3, self.g3, self.h3],
+        [self.a2, self.b2, self.c2, self.d2, self.e2, self.f2, self.g2, self.h2],
+        [self.a1, self.b1, self.c1, self.d1, self.e1, self.f1, self.g1, self.h1]]
+        '''
+        self.chess_board_list = [
+        [self.a1, self.b1, self.c1, self.d1, self.e1, self.f1, self.g1, self.h1],
+        [self.a2, self.b2, self.c2, self.d2, self.e2, self.f2, self.g2, self.h2],
+        [self.a3, self.b3, self.c3, self.d3, self.e3, self.f3, self.g3, self.h3],
+        [self.a4, self.b4, self.c4, self.d4, self.e4, self.f4, self.g4, self.h4],
+        [self.a5, self.b5, self.c5, self.d5, self.e5, self.f5, self.g5, self.h5],
+        [self.a6, self.b6, self.c6, self.d6, self.e6, self.f6, self.g6, self.h6],
+        [self.a7, self.b7, self.c7, self.d7, self.e7, self.f7, self.g7, self.h7],
+        [self.a8, self.b8, self.c8, self.d8, self.e8, self.f8, self.g8, self.h8]]
 
-        self.a2.setText('♙')
-        self.b2.setText('♙')
-        self.c2.setText('♙')
-        self.d2.setText('♙')
-        self.e2.setText('♙')
-        self.f2.setText('♙')
-        self.g2.setText('♙')
-        self.h2.setText('♙')
+        chess_piese_list = []
+        self.move_save = []
+
+        for name in self.chess_board_list[1]:
+            name.setText('♟')
+        for name1 in self.chess_board_list[6]:
+            name1.setText('♙')
+        for self.dick_index_1 in range (8):
+            for self.dick_index_a in range (8):
+                self.chess_board_list[self.dick_index_1][self.dick_index_a].clicked.connect(functools.partial(self.press_field, first_press_index_a = self.dick_index_a, first_press_index_1 = self.dick_index_1))
+
+        '''
+        for self.dick_index in range (8):
+            for self.dick in chess_board_list[self.dick_index]:
+                self.dick.clicked.connect(functools.partial(self.press_field, first_press = self.dick, first_press_index = self.dick_index))
+        '''
         #
         self.a1.setText('♖')
         self.b1.setText('♘')
@@ -97,15 +125,6 @@ class WindowPlay(QtWidgets.QMainWindow):
         self.g1.setText('♘')
         self.h1.setText('♖')
         #
-        self.a7.setText('♟')
-        self.b7.setText('♟')
-        self.c7.setText('♟')
-        self.d7.setText('♟')
-        self.e7.setText('♟')
-        self.f7.setText('♟')
-        self.g7.setText('♟')
-        self.h7.setText('♟')
-        #
         self.a8.setText('♜')
         self.b8.setText('♞')
         self.c8.setText('♝')
@@ -115,6 +134,35 @@ class WindowPlay(QtWidgets.QMainWindow):
         self.g8.setText('♞')
         self.h8.setText('♜')
 
+    def press_field(self, first_press_index_a, first_press_index_1):
+        a_to_h = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', ]
+        one_to_eight = ['1', '2', '3', '4', '5', '6', '7', '8', ]
+        print(self.chess_board_list[first_press_index_a][first_press_index_1])
+        print('{}{}'.format(a_to_h[first_press_index_a], (one_to_eight[first_press_index_1])))
+        self.move_save.append(self.chess_board_list[first_press_index_a][first_press_index_1])
+        self.check()
+
+    def check (self):
+        if len(self.move_save) == 2:
+
+            print('complete')
+            print(self.move_save)
+            self.move_save[0].setText('')
+            print('1 complete')
+            self.move_save[1].setText('хуй')
+            self.move_save.clear()
+            print('success')
+
+
+        '''
+        self.dirlist = self # !!!
+        if self.dirlist:
+            self.chess_move.setText(self.dirlist)
+        else:
+            self.lineEdit.clear()
+        '''
+    def from_to_other(self):
+        self.label.setText(self.dirlist)  # !!!
 
     def press_more_button(self):
         windowMain = WindowMain()
--- test2.py
+++ test2.py
@@ -1,36 +1,31 @@
-from PyQt5.QtWidgets import *
+from PyQt6.QtWidgets import *
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
         self.layout = QVBoxLayout(self)
-        self.someFunction(self.function)
+        self.someFunction()
     way = []
-    def someFunction(self, differ):
+    def someFunction(self):
 #       buttons = [None] * 8
         chess_board1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
         chess_board2 = [1, 2, 3, 4, 5, 6, 7, 8]
 
-            for j in range(8):
-                for i in range(8):
-                    button = QPushButton("Button {}{}".format(chess_board1[j], chess_board2[i]), self)
-                    button.clicked.connect(lambda ch, j=j, i=i: self.differ(j, i))      # < ---
-                    self.layout.addWidget(button)
+        for j in range(8):
+            for i in range(8):
+                button = QPushButton("Button {}{}".format(chess_board1[j], chess_board2[i]), self)
+                button.clicked.connect(lambda ch, j=j, i=i: self.differ(j, i))      # < ---
+                self.layout.addWidget(button)
 
-    def function(self, j ,i):
+    def differ(self, j ,i):
         chess_board1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
         chess_board2 = [1, 2, 3, 4, 5, 6, 7, 8]
-        self.way.append('{}{}'.format(chess_board1[j], chess_board2[i]))
+        print("{}{}".format(chess_board1[j], chess_board2[i]))
 
-    def function2(self, j, i):
-        chess_board1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-        chess_board2 = [1, 2, 3, 4, 5, 6, 7, 8]
-        self.way.append('{}{}'.format(chess_board1[j], chess_board2[i]))
-        print(self.way[0], self.way[1])
 
 if __name__ == '__main__':
     app = QApplication([])
     mainapp = MainWindow()
     mainapp.show()
-    app.exec_()
+    app.exec()
